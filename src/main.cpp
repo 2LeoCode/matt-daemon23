a3df985cd0d3963704123595adad014e8b043c07
lsuardi <leo.suardi@volta-medical.com>
2025-01-17 18:40:40 +0000

    feat: require root rights

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include <cstdio>
+#include <unistd.h>
+
 #include <print>
 
+#include "utils/utils.hpp"
+
 int main() {
+  if (geteuid() != 0) {
+    fatalError("this program must be ran as root");
+  }
   std::println("Hello world!");
   return 0;
 }
---
+++ src/utils/utils.hpp
@@ -0,0 +1,7 @@
+#ifndef UTILS_HPP
+
+#include <string_view>
+
+[[noreturn]] void fatalError(const std::string_view &message);
+
+#endif // !UTILS_HPP
